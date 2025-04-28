ba1d5452c5cecdb2511d37a0e91cf922988ed94c
ZANDRYN <epraudite@gmail.com>
2025-04-27 23:44:09 -0400

    Added iOs Development PreWork files to Repo.

---
+++ Background_Proj/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Background_Proj
+//
+//  Created by Zandryn Epan on 4/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            }
+        
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+}
+
